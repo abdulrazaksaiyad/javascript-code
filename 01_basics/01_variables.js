4029c6f3923c654708791051a075e9adb2a693a7
abdulrazaksaiyad <127005472+abdulrazaksaiyad@users.noreply.github.com>
2024-08-07 18:24:28 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail = "abdulrajak@google.com"
+var accountPassword = "12345"
+accountCity = "vadodra"
+let accountState;
+
+// accountId = 2  //not allowed
+
+accountEmail = "ab@ab.com"
+accountPassword = "54321"
+accountCity = "Bangaluru"
+accountState = "Karnatak"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*
+    Prefer not use var
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,6 @@
+"use strict"; //treat all JS code as newer version
+
+console(4 + 4)
+
+console("abdulrajak")
+
