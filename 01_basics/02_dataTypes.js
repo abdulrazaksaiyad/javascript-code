74918c962ab105d313d989624559dc1ab5623ef3
abdulrazaksaiyad <127005472+abdulrazaksaiyad@users.noreply.github.com>
2024-08-07 18:43:33 +0000

    dataTypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,20 @@
 "use strict"; //treat all JS code as newer version
 
-console(4 + 4)
+console.log(4 + 4)
 
-console("abdulrajak")
+console.log("abdulrajak")
+
+
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol =>unique
+
+//object
+
+console.log(typeof "Abdulrajak");
 
